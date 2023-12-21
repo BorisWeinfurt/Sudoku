ab75abc052f982d04aefb2919ac83f9eaf92527f
Boris Weinfurt <weinfub@wwu.edu>
2023-12-21 15:18:02 -0800

    implement driver functionality

--- src/boardDriver.py
+++ src/boardDriver.py
@@ -1,6 +1,6 @@
 class BoardDriver:
 
-    def __init__():
+    def __init__(self):
         #temporary until custom puzzles are implemented
         sudoku_puzzle = [
             [5, 3, 0, 0, 7, 0, 0, 0, 0],
@@ -15,37 +15,45 @@ class BoardDriver:
         ]
         self.board = sudoku_puzzle
 
-    def add_digit(Position, digit):
+    def add_digit(self, Position, digit):
         if checkPositionValid(Position):
-            board[Position.row][Position.col] = digit
+            self.board[Position.row][Position.col] = digit
             return True
         return False
 
-    def checkPositionValid(Position, digit):
+    def checkPositionValid(self, Position, digit):
         row = getRow(Position.row)
         col = getCol(Position.col)
-        box = getBox(Position.row // 3 * 3 + Position.col // 2)
+        box = getBox(Position.row // 3 * 3 + Position.col // 3)
 
         return digit not in row and digit not in col and digit not in box
 
-    def getRow(rowIndex):
-        return board[rowIndex]
+    def getRow(self, rowIndex):
+        return self.board[rowIndex]
     
-    def getCol(colIndex):
+    def getCol(self, colIndex):
         col = []
-        for row in board:
+        for row in self.board:
             col.append(row[colIndex])
+        return col
     
-    def getBox(box):
+    def getBox(self, box):
+        initRow = box // 3
+        initCol = box % 3
 
+        box_values = []
+        for row in range(initRow * 3, initRow * 3 + 3):
+            for col in range(initCol * 3, initCol * 3 + 3):
+                box_values.append(self.board[row][col])
+        return box_values
 
-    def printBoard(board):
-        for i, row in enumerate(board):
+    def printBoard(self):
+        for i, row in enumerate(self.board):
             if i % 3 == 0 and i != 0:
                 print("-"*21)  # Add horizontal line every 3 rows
             print(" ".join(map(str, row[:3])) + " | " +
                 " ".join(map(str, row[3:6])) + " | " +
                 " ".join(map(str, row[6:])))
 
-    def isComplete():
+    def isComplete(self):
         
